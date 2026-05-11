dbd6152ca2e5545e574fd3b292f97c6abc888560
Afonso <afonsogouveiad@gmail.com>
2026-05-11 11:26:01 +0200

    bd

--- db_init/create.sql
+++ db_init/create.sql
@@ -27,4 +27,80 @@ FLUSH PRIVILEGES;
 -- Després de crear la base de dades, cal seleccionar-la per treballar-hi
 USE cantina;
 
+DROP TABLE IF EXISTS `menus`;
+CREATE TABLE `menus` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `price` decimal(5,2) DEFAULT NULL,
+  `day` text COLLATE utf8mb4_unicode_ci,
+  `week` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+DROP TABLE IF EXISTS `menus2`;
+CREATE TABLE `menus2` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `price` decimal(10,0) DEFAULT NULL,
+  `day` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `week` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` decimal(65,0) NOT NULL,
+  `category` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `products` (`id`, `name`, `price`, `category`) VALUES
+(4,	'cafe',	12,	'Begudes'),
+(5,	'Tallat',	1,	'Begudes'),
+(6,	'Cafe amb Liet',	1,	'Begudes'),
+(7,	'Infusions',	1,	'Begudes'),
+(8,	'Aigua 500ml',	1,	'Begudes'),
+(9,	'Aigua 1500ml',	1,	'Begudes'),
+(10,	'Liaunes',	1,	'Begudes'),
+(11,	'Cacaulat',	2,	'Begudes'),
+(12,	'Liet soja i sense lactosa',	1,	'Begudes'),
+(13,	'Gel',	1,	'Begudes'),
+(14,	'Croissant',	1,	'Pastes'),
+(15,	'Donut Sucre',	1,	'Pastes'),
+(16,	'Donut xoco',	1,	'Pastes'),
+(17,	'Canya',	1,	'Pastes'),
+(18,	'Tonyina',	2,	'Entrepans Freds'),
+(19,	'Pernil savat',	2,	'Entrepans Freds'),
+(20,	'Fuet ',	2,	'Entrepans Freds'),
+(21,	'Formatge',	2,	'Entrepans Freds'),
+(22,	'Xoriço',	2,	'Entrepans Freds'),
+(23,	'Pernil Dolç',	2,	'Entrepans Freds'),
+(24,	'Frankfurt',	2,	'Entrepans Calents'),
+(25,	'Bacon',	2,	'Entrepans Calents'),
+(26,	'Llom',	3,	'Entrepans Calents'),
+(27,	'Hamburguesa',	3,	'Entrepans Calents'),
+(28,	'Truita',	3,	'Entrepans Calents'),
+(29,	'Mallorqui',	3,	'2'),
+(30,	'Bikini',	3,	'Entrepans Calents'),
+(31,	'Xistorra',	3,	'Entrepans Calents'),
+(32,	'Serranito',	2,	'Entrepans Calents');
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1,	'admin',	'1234');
+
+-- 2026-05-11 09:22:55 UTC
+
+
 
